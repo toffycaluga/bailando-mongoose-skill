5dd457fc833d328a9172bc72abf1433da5e2fa6a
Abraham Lillo <abraham.lillol.com>
2026-07-17 13:35:10 +0200

    feat: crear modelo de canción con validaciones

---
+++ models/Cancion.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const cancionSchema = new mongoose.Schema(
+  {
+    titulo: {
+      type: String,
+      required: [true, "El título de la canción es obligatorio"],
+      minlength: [6, "El título debe tener al menos 6 caracteres"],
+      maxlength: [255, "El título no puede superar los 255 caracteres"],
+      trim: true,
+    },
+
+    artista: {
+      type: String,
+      required: [true, "El artista es obligatorio"],
+      minlength: [10, "El artista debe tener al menos 10 caracteres"],
+      maxlength: [255, "El artista no puede superar los 255 caracteres"],
+      trim: true,
+    },
+
+    anioLanzamiento: {
+      type: Number,
+      required: [true, "El año de lanzamiento es obligatorio"],
+      min: [1000, "El año de lanzamiento debe tener 4 dígitos"],
+      max: [9999, "El año de lanzamiento debe tener 4 dígitos"],
+    },
+
+    genero: {
+      type: String,
+      required: [true, "El género musical es obligatorio"],
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Cancion = mongoose.model("Cancion", cancionSchema);
+
+module.exports = Cancion;
